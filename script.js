298135b0a7b81896bb11852384f07c8177bf29e4
ItzelAlbores <itzelbeatriz.albores.huerta2225@cbtis270.edu.mx>
2024-06-10 16:15:26 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const $tiempo = document.querySelector('.tiempo'),
+$fecha = document.querySelector('.fecha');
+
+function digitalClock(){
+    let f = new Date(),
+    dia = f.getDate(),
+    mes = f.getMonth() + 1,
+    anio = f.getFullYear(),
+    diaSemana = f.getDay()-1;
+
+    dia = ('0' + dia).slice(-2);
+    mes = ('0' + mes).slice(-2)
+
+    let timeString = f.toLocaleTimeString();
+    $tiempo.innerHTML = timeString;
+
+    let semana = ['LUNES','MARTES','MIERCOLES','JUEVES','VIERNES','SABADO','DOMINGO'];
+    let showSemana = (semana[diaSemana]);
+    $fecha.innerHTML = `${dia}-${mes}-${anio} ${showSemana}`
+    
+}
+setInterval(() => {
+    digitalClock()
+}, 1000);
